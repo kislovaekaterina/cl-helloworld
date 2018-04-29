94651fe9c79aea30a4dbb11904b15aebd080d52a
ekaterinakislova <kislova.ekaterina@gmail.com>
2018-04-29 17:45:49 +0100

    Fixed default route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/hello', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 var port = process.env.PORT || 3000;
 
